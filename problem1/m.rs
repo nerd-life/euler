2c3d4d37c7e7fe2727d784987c5020121daa534c
Dimitar Kostov <dimitar.kostov@emerchantpay.com>
2015-01-22 09:00:24 +0200

    Adds solution to problem 1

---
+++ problem1/m.rs
@@ -0,0 +1,10 @@
+use std::iter::AdditiveIterator;
+
+fn is_devisable_to_3_or_5(num: i32) -> bool {
+    num % 3 == 0 || num % 5 == 0
+}
+
+fn main() {
+    let result = range(0i32, 1000).filter(|&x| is_devisable_to_3_or_5(x)).sum();
+    println!("Sum is: {}", result);
+}
